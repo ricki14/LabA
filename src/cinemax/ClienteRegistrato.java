b72c864f6b37f1715ecc89dc1b1689e52be70df9
EdOaRdO CardVcCi <edoardo1.carducci@gmail.com>
2026-07-29 22:13:49 +0200

    impostazione della classe cliente registrato

--- src/cinemax/ClienteRegistrato.java
+++ src/cinemax/ClienteRegistrato.java
@@ -1,5 +1,29 @@
 package cinemax;
 
-public class ClienteRegistrato {
+import java.time.LocalDate;
 
+public class ClienteRegistrato extends Utente{
+
+    //costruttore
+    public ClienteRegistrato (String nome, String cognome, String username, String password,
+                              LocalDate dataDiNascita, Domicilio domicilio, Ruolo ruolo){
+        super(nome, cognome, username, password, dataDiNascita, domicilio, Ruolo.CLIENTE);
+    }
+
+    //metodi
+    public void creaPrenotazione(){
+
+    }
+
+    public void visualizzaPrenotazione(){
+
+    }
+
+    public void modificaPrenotazione(){
+
+    }
+
+    public void eliminaPrenotazione(){
+
+    }
 }
