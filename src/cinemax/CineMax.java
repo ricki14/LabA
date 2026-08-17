5d665c90c9f53a2404b5ab94d0fba5a3ec757afa
Daniele Rossetti <daniele.rossetti@supsi.ch>
2026-08-17 16:47:07 +0200

    Implementati login e registrazione nella classe CineMax

--- src/cinemax/CineMax.java
+++ src/cinemax/CineMax.java
@@ -1,7 +1,13 @@
 package cinemax;
 
+import javax.sound.midi.Soundbank;
+import java.net.ServerSocket;
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class CineMax {
@@ -21,8 +27,8 @@ public class CineMax {
 
         //menù principale
         boolean esci=false;
-        ProgrammazioneCinema programmazione = new ProgrammazioneCinema();
-        programmazione.caricaDaCSV("proiezioni.csv");
+        GestorePrenotazioni programmazione = new GestorePrenotazioni();
+        programmazione.caricaPrenotazioni("data/prenotazioni.csv");
 
         while(!esci){
 
@@ -37,12 +43,75 @@ public class CineMax {
                 scanner.nextLine();
 
                 switch (numeroScelto) {
-                    case 1:
-                        //Login
+                    case 1: {
+                        System.out.println("----LOGIN----");
+                        System.out.println("Inserisci username: ");
+                        String username = scanner.nextLine();
+                        System.out.println("Inserisci password: ");
+                        String password = scanner.nextLine();
+
+                        String passwordHash = PasswordUtil.hashPassword(password);
+                        boolean loginEffettuato = false;
+
+                        for (Utente utente : utenti) {
+                            if (utente.getUsername().equals(username) && utente.getPassword().equals(passwordHash)) {
+                                loginEffettuato = true;
+                                System.out.println("Login effettuato");
+                                break;
+                            }
+                        }
+                        if (!loginEffettuato) {
+                            System.out.println("Username o password errati");
+                        }
                         break;
-                    case 2:
-                        //Registrazione
+                    }
+                    case 2: {
+                        System.out.println("---- REGISTRAZIONE ----");
+                        System.out.println("Nome: ");
+                        String nome = scanner.nextLine();
+                        System.out.println("Cognome: ");
+                        String cognome= scanner.nextLine();
+                        System.out.println("Username: ");
+                        String username=scanner.nextLine();
+
+                        boolean usernameEsistente = false;
+                        for (Utente utente: utenti){
+                            if (utente.getUsername().equals(username)){
+                                usernameEsistente=true;
+                                break;
+                            }
+                        }
+                        if (usernameEsistente){
+                            System.out.println("Username già presente");
+                            break;
+                        }
+                        System.out.println("Password: ");
+                        String password=scanner.nextLine();
+
+                        String hashPassword=PasswordUtil.hashPassword(password);
+
+                        System.out.println("Data di nascita (AAAA-MM-GG): ");
+                        LocalDate dataDiNascita = LocalDate.parse(scanner.nextLine());
+                        System.out.println("Via: ");
+                        String via = scanner.nextLine();
+                        System.out.println("Numero civico: ");
+                        String numeroCivico = scanner.nextLine();
+                        System.out.println("CAP: ");
+                        String cap = scanner.nextLine();
+                        System.out.println("Città: ");
+                        String citta=scanner.nextLine();
+                        System.out.println("Provincia: ");
+                        String provincia = scanner.nextLine();
+
+                        Domicilio domicilio = new Domicilio(via,numeroCivico,cap,citta,provincia);
+
+                        ClienteRegistrato nuovoCliente= new ClienteRegistrato(nome,cognome,username,hashPassword,dataDiNascita,domicilio,Ruolo.CLIENTE);
+
+                        utenti.add(nuovoCliente);
+                        gestoreUtenti.salvaUtenti();
+                        System.out.println("Registrazione completata");
                         break;
+                    }
                     case 3: {
                         //Continua come ospite
                         boolean back = false;
@@ -72,18 +141,20 @@ public class CineMax {
                                         break;
                                 }
 
+                            }else {
+                                System.out.println("Inserisci un valore valido!");
+                                scanner.nextLine();
                             }
-                        } else {
-                            System.out.println("Inserisci un valore valido!");
-                            scanner.nextLine();
                         }
                     }break;
-                    case 4:
+                    case 4: {
                         esci = true;
                         break;
+                    }
                     default:
                         System.out.println("Scelta non valida");
                         break;
+
                 }
             } else {
                 System.out.println("Carattere inserito non valido!");
